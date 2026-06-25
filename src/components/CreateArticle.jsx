db2d2311ace601d818c15bae101f305157308116
Lionel Bezuidenhout <lionelbez487@gmail.com>
2026-06-25 09:40:23 +0200

    Fix case-sensitive API imports

--- src/components/CreateArticle.jsx
+++ src/components/CreateArticle.jsx
@@ -1,5 +1,5 @@
 import { useState} from 'react';
-import API from "../services/API";
+import API from "../services/api";
 import storageService from "../services/storageService";
 
 function CreateArticle({onArticleCreated }) {
